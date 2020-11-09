24bb3785be9418c6e3bf8155a0a4cdf1c9c5e515
MaxXSoft <x@MaxXSoft.net>
2020-11-09 19:23:58 +0800

    (is_leap_year) removed duplicated test cases

--- functional/52_is_leap_year.c
+++ functional/52_is_leap_year.c
@@ -1,6 +1,12 @@
-//test is leap year
-int main(){
-    int year;
-    year = 2020;
-    return (year % 400 == 0 || (year % 4 == 0) && (year % 100 != 0));
+int year;
+int flag;
+int getint();
+int main()
+{
+	year=getint();
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+		flag=1;
+	else
+		flag=0;
+	return flag;
 }
