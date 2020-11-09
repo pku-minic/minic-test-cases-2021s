34631afcea86f7e748b26346ae899a3a21c49088
MaxXSoft <x@MaxXSoft.net>
2020-11-09 18:57:40 +0800

    (functional) fixed some faults about encoding

--- functional/60_unlucky_data.c
+++ functional/60_unlucky_data.c
@@ -2,8 +2,8 @@ int main() {
 	int month[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 	int week = 7;
 	int begin;
-	if (week <= 5)//一月几号开始是周五
-		begin = 1 + 5 - week;//一月几号开始是周五
+	if (week <= 5)//涓�鏈堝嚑鍙峰紑濮嬫槸鍛ㄤ簲
+		begin = 1 + 5 - week;//涓�鏈堝嚑鍙峰紑濮嬫槸鍛ㄤ簲
 	else
 		begin = 8 - (week - 5);
 	int mon = 1;
