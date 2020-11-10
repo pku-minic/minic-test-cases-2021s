17a4b94302c96a8133e29c91af064c0ef0fddc28
MaxXSoft <x@MaxXSoft.net>
2020-11-10 19:00:34 +0800

    (scope) added a new functional test case

---
+++ functional/051_scope.c
@@ -0,0 +1,26 @@
+int k;
+
+int main() {
+  k = 3389;
+  if (k < 10000) {
+    k = k + 1;
+    int k = k * 2;
+    while (k > 10) {
+      k = k - 88;
+      if (k < 1000) {
+        int g = 9;
+        {
+          int l = 11;
+          {
+            g = 10;
+            k = k - g;
+            int g = g * 11;
+            k = k + g + l;
+          }
+        }
+      }
+    }
+    putint(k);
+  }
+  return k;
+}
