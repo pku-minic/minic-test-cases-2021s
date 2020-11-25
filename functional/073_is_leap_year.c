6e621ca8d84e699c3a5351b83d7f955c72c6ac70
MaxXSoft <x@MaxXSoft.net>
2020-11-25 20:46:51 +0800

    (073) fixed a fault about SysY syntax

--- functional/073_is_leap_year.c
+++ functional/073_is_leap_year.c
@@ -3,7 +3,9 @@ int flag;
 int main()
 {
 	year=getint();
-	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	if (year % 4 == 0 && year % 100 != 0)
+		flag=1;
+	else if (year % 400 == 0)
 		flag=1;
 	else
 		flag=0;
